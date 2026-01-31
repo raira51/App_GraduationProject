5400a1bb4198c4b71f934b775184bd601482ce54
raira <runteq.rai51@gmail.com>
2026-02-01 00:13:13 +0900

    Celebrationsにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_31_045600) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_31_121228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_31_045600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "short_message"
+    t.integer "status"
     t.index ["share_url"], name: "index_celebrations_on_share_url", unique: true
     t.index ["user_id"], name: "index_celebrations_on_user_id"
   end
