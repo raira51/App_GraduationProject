c598734b8b554a3313496e379a1efcf4ba05437e
raira <runteq.rai51@gmail.com>
2026-02-01 20:51:09 +0900

    手紙投稿機能の実装

---
+++ app/controllers/letters_controller.rb
@@ -0,0 +1,48 @@
+class LettersController < ApplicationController
+  before_action :require_login
+  before_action :set_celebration
+  before_action :set_letter, only: %i[edit update destroy]
+
+  def new
+    @letter = @celebration.letters.new
+  end
+
+  def create
+    @letter = @celebration.letters.new(letter_params)
+    if @letter.save
+      redirect_to edit_celebration_path(@celebration), notice: "手紙を追加しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @letter.update(letter_params)
+      redirect_to edit_celebration_path(@celebration), notice: "手紙を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @letter.destroy!
+    redirect_to edit_celebration_path(@celebration), notice: "手紙を削除しました"
+  end
+
+  private
+
+  def set_celebration
+    @celebration = current_user.celebrations.find(params[:celebration_id])
+  end
+
+  def set_letter
+    @letter = @celebration.letters.find(params[:id])
+  end
+
+  def letter_params
+    params.require(:letter).permit(:sender_name, :body)
+  end
+end
